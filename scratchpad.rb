69cfe13e1d81166351275e54d48e8d610baaec7a
Nikhil Math <erulemath@gmail.com>
2020-11-29 18:10:56 -0700

    initial Push

---
+++ scratchpad.rb
@@ -0,0 +1,5 @@
+puts "Hello can I have your name?"
+name = gets.chomp
+puts "hello #{name}, nice to meet you, what is your age?"
+age = gets.chomp.to_i
+puts "Hello #{name}, you are #{age} years old."
