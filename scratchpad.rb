ef760880f9daa1970db8ca4074a59537e29dace4
Nikhil Math <erulemath@gmail.com>
2020-11-29 18:31:33 -0700

    did 29

--- scratchpad.rb
+++ scratchpad.rb
@@ -1,5 +1,5 @@
-puts "Hello can I have your name?"
-name = gets.chomp
-puts "hello #{name}, nice to meet you, what is your age?"
-age = gets.chomp.to_i
-puts "Hello #{name}, you are #{age} years old."
+p '5'.to_s.class
+
+p 10.to_i.class
+
+p 9.99.to_f.class
