c9594d38f7258cbf8e07c3a1a855ef585f2942b5
Nikhil Math <erulemath@gmail.com>
2020-11-30 17:04:51 -0700

    chlannwege 39 done

---
+++ challenge39.rb
@@ -0,0 +1,5 @@
+#Use the times method to output the first 10 multiples of 3 (3, 6, 9, 12, ...... 30)
+
+10.times do |count|
+puts "#{(count + 1) * 3}"
+end
--- scratchpad.rb
+++ scratchpad.rb
@@ -1,5 +1,46 @@
-p 'cat'.between?('ant','dog')
+10.times { |count| puts "We are on numbe r #{count}"}
 
-p 20.between?(1,23)
+# 3.times do |you|
+#   puts "We are currently on loop #{you + 1}"
+#   puts "Nikhil is cool"
+#   puts "I am having so much fun learning ruby!"
+# end
 
-p 1.2.between?(1, 2)
+p count
+
+
+# a = 10
+
+# # a = a + 5
+# a += 5
+
+# p a
+
+# b = 100
+# p b -=50
+
+
+# c = 3
+# p c 
+
+# # c = c * 4
+# c *=4
+# p c
+
+# d = 5
+# p d
+
+# # d = d / 3.000
+# d /= 3.0
+# p d
+
+
+# p 10.9.to_i.class
+
+# p 10.5.floor
+
+# p 10.5.ceil.class
+
+# p 3.14159.round(4)
+
+# p 35.67.abs
