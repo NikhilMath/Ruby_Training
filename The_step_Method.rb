d18cf471e23445541344d7a2e533f467466e2ea8
Nikhil Math <erulemath@gmail.com>
2020-11-30 17:25:02 -0700

    did two sections

---
+++ The_step_Method.rb
@@ -0,0 +1,5 @@
+0.step(85, 7) do |num|
+  puts  "we are going up by 7"  
+  puts "this is the #{num}"
+  sleep(0.25)
+end
---
+++ theUptoAndDownto.rb
@@ -0,0 +1,14 @@
+5.upto(10) do |current_number|
+  puts "#{current_number}"
+  sleep(1)
+end
+
+5.downto(1) do |current_number|
+  puts "#{current_number}"
+  sleep(1)
+end
+
+
+puts "liftoff"
+
+# 5.downto(-1) { |i| puts "Countdown: #{i}"}
