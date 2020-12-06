657ff108f272043652688648125a98577aa2ce5a
Nikhil Math <erulemath@gmail.com>
2020-12-06 13:27:24 -0700

    did 60

---
+++ localVariable.rb
@@ -0,0 +1,9 @@
+expression = "i am handsome"
+
+def intro
+  expression = "I am a genius"
+  puts expression
+end
+
+intro
+puts expression
---
+++ methodParametersAndArguments.rb
@@ -0,0 +1,14 @@
+def praise_person(name, age)
+  puts "#{name} is amazing"
+  puts "#{name} is charming"
+  puts "#{name} is talented"
+  puts "his age is #{age + 5}"
+end
+
+praise_person("Nikhil", 25)
+sleep(1)
+praise_person("Dave", 89)
+sleep(1)
+praise_person("Pizza", 67)
+sleep(1)
+
