65cbc15a9078828cf637d8a0d8250dbafa7eae1a
Nikhil Math <erulemath@gmail.com>
2020-12-06 13:41:51 -0700

    62 done

---
+++ returnValue.rb
@@ -0,0 +1,9 @@
+def add_to_numbers(num1, num2)
+  puts "Ok, I am solving your math problem!"
+  return num1 + num2
+  # puts "whatever"
+  #whatever will not print because it is after the return, and return stops and ends the method. 
+end
+ 
+p add_to_numbers(3,5)
+p add_to_numbers(12,8)
---
+++ returntworb.rb
@@ -0,0 +1,5 @@
+def return_guess
+  puts "blah blah blah"
+end
+
+p return_guess
