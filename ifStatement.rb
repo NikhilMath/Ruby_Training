6f1b0690767ca08580b8146148a0805ac183f333
Nikhil Math <erulemath@gmail.com>
2020-12-06 13:50:42 -0700

    63 done

---
+++ ifStatement.rb
@@ -0,0 +1,20 @@
+# # if 5 < 2
+# #  puts "that math statement is true!"
+# # end
+
+# word = "kangaroo"
+
+# if word.length == 8
+#   puts "that word has 8 letters."
+# end
+
+# word = 'kanghagreo'
+
+# if word.include?("eb")
+#   puts "yup, your words has eb in it!"
+# end
+
+
+if 6.odd?
+  puts "that number is odd."
+end
