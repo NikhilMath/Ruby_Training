b3e013110653b8d77abb3b028edfb68736b022b1
Nikhil Math <erulemath@gmail.com>
2020-12-07 16:20:45 -0700

    now I did ti

---
+++ ternaryOperator.rb
@@ -0,0 +1,38 @@
+
+pokemon = "pikachu"
+
+def check_pokemon(check_pokemon)
+  pokemon == "charizard" ? "Fireball" : "that is not charizrd"
+end
+
+p check_pokemon("onix")
+p check_pokemon("apple")
+p check_pokemon("charazard")
+
+
+
+# def even_or_odd(number)
+#   number.even? ? "even" : "odd"
+# end
+
+# puts even_or_odd(5)
+
+
+# if "yes" == "yes"
+#   puts "The two are equal"
+# else
+#   puts "The two are not equal"
+# end
+
+# puts "yes" == "yes" ? "Yes" : "no"
+
+# if 1 < 2
+#   puts "Yes, it is!"
+# else
+#   puts "No, it is not!"
+# end
+
+
+# puts 1 < 2 ? "Yes it is!" : "No it is not!"
+
+# This one line lowers the amount of the 5 lined thing. 
