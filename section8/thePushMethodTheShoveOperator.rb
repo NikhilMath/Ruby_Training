649a87ea67db81c16d6f91780f6d38b5d610a51e
Nikhil Math <erulemath@gmail.com>
2021-02-01 13:55:32 -0700

    i learned insert, push, and shovel method.

---
+++ section8/thePushMethodTheShoveOperator.rb
@@ -0,0 +1,11 @@
+location = ["house", "airport", "bar"]
+
+# location.push("restruaunt", "saloon")
+
+# location << "applebees" << "chillies"
+p location
+
+location.insert(1, "restraunt", "applebees", "cafe") 
+
+
+p location
