2516383510c545063bb650e83f28fede06a10a27
Nikhil Math <erulemath@gmail.com>
2021-02-07 15:24:13 -0700

    learmed blocks

--- section8/theIsA?Method.rb
+++ section8/theIsA?Method.rb
@@ -1,2 +1,4 @@
 
 p 1.is_a?(Bignum)
+
+p [1, 2, 3, 4, 5].first(1)
---
+++ section9/review_of_blocks.rb
@@ -0,0 +1,6 @@
+3.times { |number| puts "I am currently on loop number #{number + 1}"}
+
+5.times do |number|
+square = number * number
+  puts "the current number is #{number} and the sqaure = #{square}"
+end
