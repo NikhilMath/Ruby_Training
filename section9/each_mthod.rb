b468ba6bd416d407d4647bfa9d7c872cc811df86
Nikhil Math <erulemath@gmail.com>
2021-02-07 18:40:21 -0700

    .each method

---
+++ section9/each_mthod.rb
@@ -0,0 +1,16 @@
+# candies = ["souu Patch kids", "milky way", "airheads"]
+
+# candies.each do |candy|
+#   puts "I love eaching #{candy}"
+#   puts "It takes so good"
+# end
+
+
+# names = [ "bo", "moe", "joe"]
+
+# names.each { |name| puts name.upcase }
+
+[1, 2, 3, 4, 5, 6].each do |num|
+  square = num * num
+  puts "The Square of #{num} is #{square}"
+end
